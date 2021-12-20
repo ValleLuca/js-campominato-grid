8ef1251c087714c864d8bd01cc49a809fad4dfa6
ValleLuca <valle.lucaAb@outlook.it>
2021-12-20 17:34:35 +0100

    Aggiunto codice js per selezionare il livello

---
+++ myscript.js
@@ -0,0 +1,15 @@
+let pulsante = document.getElementById("msBottone");
+let livelloSelezionato = document.getElementById("livello");
+let spawnGriglia = document.getElementById("griglia");
+
+pulsante.addEventListener("click", function(){
+    difficolta = livelloSelezionato.value;
+    console.log(difficolta);
+})
+
+
+
+
+
+// <div class="col border border-dark p-4">1</div>
+ 
