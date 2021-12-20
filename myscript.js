52b293dbda7488460bf7dbede4c7d3fd0e776192
ValleLuca <valle.lucaAb@outlook.it>
2021-12-20 18:14:52 +0100

    Alla selezione compaiono le griglie

--- myscript.js
+++ myscript.js
@@ -5,11 +5,29 @@ let spawnGriglia = document.getElementById("griglia");
 pulsante.addEventListener("click", function(){
     difficolta = livelloSelezionato.value;
     console.log(difficolta);
+
+    if(difficolta == "base"){
+        console.log("gg");
+        for(let i = 1; i <= 100; i++){
+        spawnGriglia.innerHTML += '<div class="col text-center border border-dark p-4">' + i + '</div>';
+        }
+    }
+    else if(difficolta == "intermedio"){
+        for(let i = 1; i <= 81; i++){
+            spawnGriglia.innerHTML += '<div class="col text-center border border-dark p-4">' + i + '</div>';
+        }
+    }
+    else
+    {
+        for(let i = 1; i <= 49; i++){
+            spawnGriglia.innerHTML += '<div class="col text-center border border-dark p-4">' + i + '</div>';
+        }
+    }
 })
 
 
 
 
 
-// <div class="col border border-dark p-4">1</div>
+// 
  
